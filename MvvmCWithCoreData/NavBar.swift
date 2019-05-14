1c5599f1f788a82bc439c5a1bc600f267d595403
Evren Yaşar <evrenyasar@Evren-MacBook-Pro.local>
2019-05-14 13:54:39 +0300

    navbar added

---
+++ MvvmCWithCoreData/NavBar.swift
@@ -0,0 +1,10 @@
+//
+//  NavBar.swift
+//  MvvmCWithCoreData
+//
+//  Created by Evren Yaşar on 14.05.2019.
+//  Copyright © 2019 Evren Yaşar. All rights reserved.
+//
+
+import Foundation
+class NavBar {}
